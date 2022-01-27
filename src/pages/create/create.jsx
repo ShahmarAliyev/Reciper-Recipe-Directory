401dd5c6449dc39738bd9b528f7be01aa27ade77
ShahmarALiyev <saliyev2019@gmail.com>
2022-01-27 15:09:09 -0600

    switched from json server to firebase firestore . Added live create,delete, update data methods.

--- src/pages/create/create.jsx
+++ src/pages/create/create.jsx
@@ -1,6 +1,6 @@
-import { useState, useRef, useEffect } from "react";
-import { useFetch } from "../../hooks/useFetch";
+import { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import { projectFirestore } from "../../firebase/config";
 
 // styles
 import "./create.css";
@@ -13,20 +13,22 @@ export default function Create() {
   const [ingredients, setIngredients] = useState([]);
   const ingredientInput = useRef(null);
 
-  const { postData, data } = useFetch(
-    "https://my-json-server.typicode.com/shahmaraliyev/db.json-for-Reciper/recipes",
-    "POST"
-  );
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    postData({
+    const doc = {
       title,
       ingredients,
       method,
       cookingTime: cookingTime + " minutes",
-    });
+    };
+    try {
+      await projectFirestore.collection("recipes").add(doc);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const handleAdd = (e) => {
@@ -41,11 +43,6 @@ export default function Create() {
   };
 
   // redirect the user when we get data response
-  useEffect(() => {
-    if (data) {
-      navigate("/");
-    }
-  }, [data, navigate]);
 
   return (
     <div className="create">
